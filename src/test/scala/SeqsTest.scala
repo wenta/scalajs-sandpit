2a99eb431b3e9314fca3b76965755d7ca67dc3f7
wenta <jakubwenta@gmail.com>
2018-03-09 11:16:31 +0100

    Create module with operation on Sequences and tests for it.

--- src/test/scala/SeqsTest.scala
+++ src/test/scala/SeqsTest.scala
@@ -1,3 +1,22 @@
-class SeqsTest {
+import org.scalatest.{FlatSpec, Matchers}
 
+class SeqsTest extends FlatSpec with Matchers {
+
+  "Seqs" should "sum two sequences" in {
+    val s1 = Seq(1.0, 1.0, 1.0)
+    val s2 = Seq(1.0, 2.0, 3.0)
+    Seqs.sum(s1, s2) shouldBe Seq(2.0, 3.0, 4.0)
+  }
+
+  it should "multiply two sequences" in {
+    val s1 = Seq(1.0, 2.0, 3.0)
+    val s2 = Seq(1.0, 2.0, 3.0)
+    Seqs.multiply(s1, s2) shouldBe Seq(1.0, 4.0, 9.0)
+  }
+
+  it should "find diff for two sequences" in {
+    val s1 = Seq(1.0, 1.0, 1.0)
+    val s2 = Seq(1.0, 2.0, 3.0)
+    Seqs.diff(s1, s2) shouldBe Seq(0.0, 1.0, 2.0)
+  }
 }
